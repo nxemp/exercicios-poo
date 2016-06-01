6bcbf40a3e68bc969697402682d1ed5b6c0b0f93
wallysson de lima <wallyssonlds@gmail.com>
2016-06-01 14:32:34 -0300

    updating 02 - add debugtmp

--- exe-04/02.py
+++ exe-04/02.py
@@ -14,17 +14,22 @@ def calcula_d1():
 
     return str(d1)
 
-cpf_tmp = cpf[1:8] + str(calcula_d1())
 def calcula_d2():
+    cpf_tmp = cpf[1:8]
+    cpf_tmp += calcula_d1()
     cont, soma = 10, 0
     for tmp in cpf_tmp:
         soma += cont * int(tmp)
+        print('soma', soma)
         cont -= 1
+        print('cont', cont)
+    soma += int(calcula_d1()) * 2
     rest = soma % 11
+
     if rest == 0 or rest == 1:
         d2 = 0
     else:
         d2 = 11 - rest
     return str(d2)
 
-print('CPF final: %s' % (cpf + calcula_d1() + calcula_d2()))
+print(calcula_d1(), calcula_d2())
