1f32b5598f887a1844941741aa7b7c46f9949244
unknown <20151144010321@PAR191908.ifrn.local>
2016-06-01 07:45:27 -0300

    add *

--- exe-04/01.py
+++ exe-04/01.py
@@ -1,13 +1,14 @@
 quest = int(input('Número de questões (1 ≤ N ≤ 80): '))
 gabarito = list()
 
+
 for tmp in range(quest):
 	gabarito.append(input('Gabarito, questão %d: ' % (tmp+1)))
 
 acertos = 0
 for tmp in range(quest):
 	resposta = input('Digite a resposta %d: ' % (tmp+1))
-	if resposta == gabarito[quest]:
+	if resposta == gabarito[tmp]:
 		acertos += 1
 
-print('Você acertou %d questões.' % quest)
+print('Você acertou %d questões.' % acertos)
